7092f8396875a4a1b6a6c1d10b2dab8bb967cc8b
AL-IMAM MEHEDI <alimammehedi@gmail.com>
2021-11-06 16:47:20 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+void main(){
+
+var pro_name = ("Quiz test");
+var name = ("Al Imam mehedi");
+var batch = ("AB23-0, Gopalganj-01");
+var trainer_name = ("Md Biplob Sheikh");
+
+
+print("This is ${pro_name}. My name is ${name}. My batch name is ${batch}. Our trainer name is ${trainer_name}.");
+
+}
